6d49fa47bfe43db2f96e1b305a6776575bb101dc
khalil <khalil.stessens@ing.austral.edu.ar>
2021-07-05 11:51:40 -0300

    first attempt

--- src/main/java/group7/jibberjabber/messages/controllers/ConversationController.java
+++ src/main/java/group7/jibberjabber/messages/controllers/ConversationController.java
@@ -1,15 +1,14 @@
 package group7.jibberjabber.messages.controllers;
 
+import group7.jibberjabber.messages.dtos.ConversationDto;
 import group7.jibberjabber.messages.dtos.MessageDto;
-import group7.jibberjabber.messages.models.Message;
+import group7.jibberjabber.messages.dtos.ResponseMessageDto;
 import group7.jibberjabber.messages.services.ConversationService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 
@@ -19,22 +18,20 @@ public class ConversationController {
 
     private final ConversationService conversationService;
 
+    @Autowired
     public ConversationController(ConversationService conversationService) {
         this.conversationService = conversationService;
     }
 
-    @Autowired
-
-
     @MessageMapping()
-    @SendTo("/queue/messages")
-    public Message processMessage(@Payload MessageDto messageDto) {
+    @SendTo("/topic/messages")
+    public List<ResponseMessageDto> processMessage(@Payload MessageDto messageDto) {
         return conversationService.addMessage(messageDto);
     }
 
-    @GetMapping("/{senderId}/{recipientId}/messages")
-    public List<Message> findConversationMessages(@PathVariable String senderId, @PathVariable String recipientId) {
-        return conversationService.getConversationMessages(senderId, recipientId);
+    @GetMapping("/{senderId}/{recipientId}")
+    public ConversationDto findConversation(@PathVariable String senderId, @PathVariable String recipientId) {
+        return conversationService.getConversation(senderId, recipientId);
     }
 
 }
--- src/main/java/group7/jibberjabber/messages/services/ConversationService.java
+++ src/main/java/group7/jibberjabber/messages/services/ConversationService.java
@@ -1,6 +1,8 @@
 package group7.jibberjabber.messages.services;
 
+import group7.jibberjabber.messages.dtos.ConversationDto;
 import group7.jibberjabber.messages.dtos.MessageDto;
+import group7.jibberjabber.messages.dtos.ResponseMessageDto;
 import group7.jibberjabber.messages.models.Conversation;
 import group7.jibberjabber.messages.models.Message;
 import group7.jibberjabber.messages.repositories.ConversationsRepository;
@@ -13,12 +15,13 @@ import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 @Transactional
 public class ConversationService {
-    private ConversationsRepository conversationsRepository;
-    private MessagesRepository messagesRepository;
+    private final ConversationsRepository conversationsRepository;
+    private final MessagesRepository messagesRepository;
 
     @Autowired
     public ConversationService(ConversationsRepository conversationsRepository, MessagesRepository messagesRepository) {
@@ -26,8 +29,9 @@ public class ConversationService {
         this.messagesRepository = messagesRepository;
     }
 
-    public Message addMessage(MessageDto messageDto){
+    public List<ResponseMessageDto> addMessage(MessageDto messageDto){
         Conversation conversation;
+
         Optional<Conversation> optional = findConversation(messageDto.getSenderId(), messageDto.getRecipientId());
         if (optional.isEmpty()) conversation = conversationsRepository.save(new Conversation(messageDto.getSenderId(),messageDto.getRecipientId()));
         else conversation = optional.get();
@@ -36,15 +40,14 @@ public class ConversationService {
         conversation.addMessage(message);
         message.setConversation(conversation);
         conversationsRepository.save(conversation);
-        return message;
+
+        return getConversationMessages(conversation.getSenderId(), conversation.getRecipientId());
     }
 
     private Optional<Conversation> findConversation(String senderId, String recipientId){
         Optional<Conversation> optional = conversationsRepository.findBySenderIdAndRecipientId(senderId, recipientId);
         if(optional.isPresent()) return optional;
-        else {
-            return conversationsRepository.findBySenderIdAndRecipientId(recipientId, senderId);
-        }
+        else return conversationsRepository.findBySenderIdAndRecipientId(recipientId, senderId);
     }
 
     private Message dtoToMessage(MessageDto messageDto){
@@ -55,16 +58,28 @@ public class ConversationService {
         return message;
     }
 
-    public List<Message> getConversationMessages(String senderId, String recipientId){
+    public List<ResponseMessageDto> getConversationMessages(String senderId, String recipientId){
         Optional<Conversation> conversation = findConversation(senderId,recipientId);
+
         if(conversation.isPresent()){
             List<Message> messages = conversation.get().getMessages();
             messages.sort(Comparator.comparing(Message::getTime).reversed());
-            return messages;
+            return messages.stream().map(ResponseMessageDto::toDto).collect(Collectors.toList());
         }
-        return new ArrayList<Message>();
+
+        return new ArrayList<>();
     }
 
 
+    public ConversationDto getConversation(String senderId, String recipientId) {
+        Conversation conversation;
+        Optional<Conversation> optionalConversation = findConversation(senderId,recipientId);
+        if(optionalConversation.isEmpty()) {
+            conversation = new Conversation(senderId,recipientId);
+            conversation = conversationsRepository.save(conversation);
+        }
+        else conversation = optionalConversation.get();
+        return ConversationDto.toDto(conversation);
+    }
 }
 
