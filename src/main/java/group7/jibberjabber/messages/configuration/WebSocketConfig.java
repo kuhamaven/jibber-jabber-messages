a48f07b33b647177bbf25e125263bc117fdf9b0a
Sofia Sidañez <sofia.sidanez@ing.austral.edu.ar>
2021-07-06 10:17:56 -0300

    web socket config

--- src/main/java/group7/jibberjabber/messages/configuration/WebSocketConfig.java
+++ src/main/java/group7/jibberjabber/messages/configuration/WebSocketConfig.java
@@ -13,8 +13,8 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/chat");
-        registry.addEndpoint("/chat").withSockJS();
-//      .setAllowedOrigins("http://localhost:3000", "http://jibber-jabber-proxy")
+        registry.addEndpoint("/chat")
+         .setAllowedOrigins("http://localhost:3000", "http://jibber-jabber-proxy").withSockJS();
     }
 
     @Override
